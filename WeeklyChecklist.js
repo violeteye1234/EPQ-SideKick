0faaa878e63d0231293b35fdbf93b74e291c3753
violeteye1234 <62634585+violeteye1234@users.noreply.github.com>
2021-03-02 23:12:46 +0800

    homepage css

--- WeeklyChecklist.js
+++ WeeklyChecklist.js
@@ -14,7 +14,7 @@ function whenLoad(){
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
 }
-  var close = document.getElementsByClassName("close");
+  close = document.getElementsByClassName("close");
   var i;
   for (i = 0; i < close.length; i++) {
       close[i].onclick = function() {
